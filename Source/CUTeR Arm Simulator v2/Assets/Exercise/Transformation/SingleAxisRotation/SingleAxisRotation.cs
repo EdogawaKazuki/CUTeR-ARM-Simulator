5974122493b76fa6c12c1672e4d025ab4ca7fc24
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2026-01-16 18:15:02 +0800

    Update Exercise: - 2D Rotation moves Joint 2 + show relative frames - Single Axis Rotation added Page 2

--- Source/CUTeR Arm Simulator v2/Assets/Exercise/Transformation/SingleAxisRotation/SingleAxisRotation.cs
+++ Source/CUTeR Arm Simulator v2/Assets/Exercise/Transformation/SingleAxisRotation/SingleAxisRotation.cs
@@ -9,18 +9,25 @@ public class SingleAxisRotation : MonoBehaviour
     RobotController _robotController;
     TMP_Text[][] _matrixText;
     Button JointSetBtn1;
+    Toggle PageToggle;
     float Angle1Sin;
     float Angle1Cos;
     float Angle2Sin;
     float Angle2Cos;
     float Angle3Sin;
     float Angle3Cos;
+    float Angle4Sin;
+    float Angle4Cos;
+    float Angle5Sin;
+    float Angle5Cos;
+    float Angle6Sin;
+    float Angle6Cos;
     // Start is called before the first frame update
     void OnEnable()
     {
-        _matrixText = new TMP_Text[3][];
+        _matrixText = new TMP_Text[6][];
         _robotController = GameObject.Find("Robot").GetComponent<RobotController>();
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 6; i++)
         {
             _matrixText[i] = new TMP_Text[9];
             for (int j = 0; j < 9; j++)
@@ -28,8 +35,28 @@ public class SingleAxisRotation : MonoBehaviour
                 _matrixText[i][j] = transform.Find("Input/Line" + (i + 2) + "/Values/" + (j + 1)).GetComponent<TMP_Text>();
             }
         }
-        JointSetBtn1 = transform.Find("Input/Line5/Set1").GetComponent<Button>();
+        JointSetBtn1 = transform.Find("Input/Line8/Set1").GetComponent<Button>();
         JointSetBtn1.onClick.AddListener(() => SetJointAngles(new(){ 90, 50, -130 }));
+        PageToggle = transform.Find("Input/Line8/PageToggle").GetComponent<Toggle>();
+        PageToggle.onValueChanged.AddListener((value) => TogglePage(value));
+    }
+
+    void TogglePage(bool isOn)
+    {
+        if(isOn){
+            for(int i = 0; i < 3; i++)
+            {
+                transform.Find("Input/Line" + (i + 2)).gameObject.SetActive(false);
+                transform.Find("Input/Line" + (i + 5)).gameObject.SetActive(true);
+            }
+
+        } else {
+            for(int i = 0; i < 3; i++)
+            {
+                transform.Find("Input/Line" + (i + 2)).gameObject.SetActive(true);
+                transform.Find("Input/Line" + (i + 5)).gameObject.SetActive(false);
+            }
+        }
     }
     private void FixedUpdate()
     {
@@ -43,6 +70,13 @@ public class SingleAxisRotation : MonoBehaviour
         Angle2Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(1));
         Angle3Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(2));
         Angle3Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(2));
+
+        Angle4Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(3));
+        Angle4Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(3));
+        Angle5Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(4));
+        Angle5Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(4));
+        Angle6Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(5));
+        Angle6Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(5));
 
         _matrixText[0][0].text = (Angle1Cos + 0.00001f).ToString("F3");
         _matrixText[0][1].text = (-Angle1Sin + 0.00001f).ToString("F3");
@@ -74,6 +108,37 @@ public class SingleAxisRotation : MonoBehaviour
         _matrixText[2][6].text = "0";
         _matrixText[2][7].text = (Angle3Sin + 0.00001f).ToString("F3");
         _matrixText[2][8].text = (Angle3Cos + 0.00001f).ToString("F3");
+
+        _matrixText[3][0].text = "1";
+        _matrixText[3][1].text = "0";
+        _matrixText[3][2].text = "0";
+        _matrixText[3][3].text = "0";
+        _matrixText[3][4].text = (Angle4Cos + 0.00001f).ToString("F3");
+        _matrixText[3][5].text = (-Angle4Sin + 0.00001f).ToString("F3");
+        _matrixText[3][6].text = "0";
+        _matrixText[3][7].text = (Angle4Sin + 0.00001f).ToString("F3");
+        _matrixText[3][8].text = (Angle4Cos + 0.00001f).ToString("F3");
+
+        _matrixText[4][0].text = (Angle5Cos + 0.00001f).ToString("F3");
+        _matrixText[4][1].text = "0";
+        _matrixText[4][2].text = (Angle5Sin + 0.00001f).ToString("F3");
+        _matrixText[4][3].text = "0";
+        _matrixText[4][4].text = "1";
+        _matrixText[4][5].text = "0";
+        _matrixText[4][6].text = (-Angle5Sin + 0.00001f).ToString("F3");
+        _matrixText[4][7].text = "0";
+        _matrixText[4][8].text = (Angle5Cos + 0.00001f).ToString("F3");
+
+        
+        _matrixText[5][0].text = "1";
+        _matrixText[5][1].text = "0";
+        _matrixText[5][2].text = "0";
+        _matrixText[5][3].text = "0";
+        _matrixText[5][4].text = (Angle6Cos + 0.00001f).ToString("F3");
+        _matrixText[5][5].text = (-Angle6Sin + 0.00001f).ToString("F3");
+        _matrixText[5][6].text = "0";
+        _matrixText[5][7].text = (Angle6Sin + 0.00001f).ToString("F3");
+        _matrixText[5][8].text = (Angle6Cos + 0.00001f).ToString("F3");
     }
 
     public void SetJointAngles(List<float> joints)
--- Source/CUTeR Arm Simulator v2/Assets/Exercise/Transformation/TwoDRotation/TwoDRotation.cs
+++ Source/CUTeR Arm Simulator v2/Assets/Exercise/Transformation/TwoDRotation/TwoDRotation.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class TwoDRotation : MonoBehaviour
 {
     RobotController _robotController;
+    RobotJointController _robotJointController;
     [SerializeField]
     LineRenderer _blueLine;
     [SerializeField]
@@ -27,14 +28,16 @@ public class TwoDRotation : MonoBehaviour
 
     Button JointSetBtn1;
     Button JointSetBtn2;
-    float Angle1Sin;
-    float Angle1Cos;
+    float Angle2Sin;
+    float Angle2Cos;
     Vector3 _redLineStart;
     Vector3 _redLineEnd;
     // Start is called before the first frame update
     void OnEnable()
     {
         _robotController = GameObject.Find("Robot").GetComponent<RobotController>();
+        _robotJointController = GameObject.Find("Robot/Joints").GetComponent<RobotJointController>();
+
         _jointTransformList = _robotController.GetJointsTransform();
         matrix1aText = transform.Find("Input/Line2/Values/a").GetComponent<TMP_Text>();
         matrix1bText = transform.Find("Input/Line2/Values/b").GetComponent<TMP_Text>();
@@ -43,21 +46,31 @@ public class TwoDRotation : MonoBehaviour
         JointSetBtn1 = transform.Find("Input/Line3/Set1").GetComponent<Button>();
         JointSetBtn2 = transform.Find("Input/Line4/Set2").GetComponent<Button>();
         JointSetBtn1.onClick.AddListener(() => SetJointAngles(new(){ 30, 180, -140 }));
-        JointSetBtn2.onClick.AddListener(() => SetJointAngles(new(){ -60, 180, -140 }));
+        JointSetBtn2.onClick.AddListener(() => SetJointAngles(new(){ 30, 100, -140 }));
         UpdateTable();
+
+        _robotJointController.ShowJointBaseFrame(true);
+        _robotJointController.ShowJointFrame(1, true);
     }
+
+    void OnDisable()
+    {
+        _robotJointController.ShowJointBaseFrame(false);
+        _robotJointController.ShowJointFrame(1, false);
+    }
+
     private void FixedUpdate()
     {
         UpdateTable();
     }
     void UpdateTable()
     {
-        Angle1Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(0));
-        Angle1Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(0));
-        matrix1aText.text = (Angle1Cos + 0.00001f).ToString("F3");
-        matrix1bText.text = (-Angle1Sin + 0.00001f).ToString("F3");
-        matrix1cText.text = (Angle1Sin + 0.00001f).ToString("F3");
-        matrix1dText.text = (Angle1Cos + 0.00001f).ToString("F3");
+        Angle2Sin = Mathf.Sin(Mathf.Deg2Rad * _robotController.GetJointAngle(1));
+        Angle2Cos = Mathf.Cos(Mathf.Deg2Rad * _robotController.GetJointAngle(1));
+        matrix1aText.text = (Angle2Cos + 0.00001f).ToString("F3");
+        matrix1bText.text = (-Angle2Sin + 0.00001f).ToString("F3");
+        matrix1cText.text = (Angle2Sin + 0.00001f).ToString("F3");
+        matrix1dText.text = (Angle2Cos + 0.00001f).ToString("F3");
 
         _redLineStart = _jointTransformList[1].position;
         _redLineEnd = _jointTransformList[1].position - _jointTransformList[1].forward * 10;
