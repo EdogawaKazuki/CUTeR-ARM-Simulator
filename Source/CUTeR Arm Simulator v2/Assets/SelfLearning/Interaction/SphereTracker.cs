70df0740dfeb14a083ca4a4545d6b247238606e0
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2026-03-15 22:04:27 +0800

    - Enabled 3DOF Interaction Demo

--- Source/CUTeR Arm Simulator v2/Assets/SelfLearning/Interaction/SphereTracker.cs
+++ Source/CUTeR Arm Simulator v2/Assets/SelfLearning/Interaction/SphereTracker.cs
@@ -21,9 +21,12 @@ public class SphereTracker : MonoBehaviour
     private UnityEngine.Vector3 velocity;
     private UnityEngine.Vector3 planeNormal = new UnityEngine.Vector3(1, 0, 0);
     private UnityEngine.Vector3 baseOffsetVector = new UnityEngine.Vector3(0.0f, 0.12f, 0.0f);
-    UnityEngine.Vector3 localOffset = new UnityEngine.Vector3(0.0f, 0.0f, 0.05f);
+    UnityEngine.Vector3 localOffset = new UnityEngine.Vector3(0.0f, 0.0f, 0.1f);
 
     private float maxLength = 0.65f;
+    private bool followJoyStick = false;
+    private UnityEngine.Vector3 lastJoyStickPosition;
+    private UnityEngine.Vector3 deltaPosition = UnityEngine.Vector3.zero;
 
     void Start()
     {
@@ -86,13 +89,50 @@ public class SphereTracker : MonoBehaviour
         if (sphereTransform == null) return;
         // if (_generalRobotControl._robotClient.IsConnected())
         // {
+        //     // Check if both right controller triggers are pressed
+        //     bool rightIndexTrigger = OVRInput.Get(OVRInput.RawButton.RIndexTrigger);
+        //     bool rightHandTrigger = OVRInput.Get(OVRInput.RawButton.RHandTrigger);
+
+        //     if (rightIndexTrigger && rightHandTrigger)
+        //     {
         //     var ctrRightPose = OVRPlugin.GetNodePoseStateImmediate(OVRPlugin.Node.ControllerRight).Pose.ToOVRPose();
 
         //     // Apply local offset first, then transform by right pose
-        //     var offsetPosition = ctrRightPose.position + ctrRightPose.orientation * localOffset;
-        //     sphereTransform.position = offsetPosition;
+        //     var currentPosition = ctrRightPose.position + ctrRightPose.orientation * localOffset;
+        //     sphereTransform.position = currentPosition;
         //     sphereTransform.rotation = ctrRightPose.orientation;
+        //     }
         // }
+
+
+        bool rightIndexTrigger = OVRInput.Get(OVRInput.RawButton.RIndexTrigger);
+        bool rightHandTrigger = OVRInput.Get(OVRInput.RawButton.RHandTrigger);
+        bool rightHandButtonOne = OVRInput.Get(OVRInput.Button.One);
+        bool rightHandButtonTwo = OVRInput.Get(OVRInput.Button.Two);
+
+        if (rightIndexTrigger && rightHandTrigger)
+        {
+            if (!followJoyStick)
+            {
+                Debug.Log("Start following joystick");
+            }
+            // get the delta position of the controller since last frame
+            var currentPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            deltaPosition = currentPosition - lastJoyStickPosition;
+            float scaleFactor = 1.5f; // Adjust this factor to control the sensitivity of the movement
+            if(rightHandButtonOne && rightHandButtonTwo)
+                deltaPosition *= scaleFactor;
+            if(deltaPosition.magnitude > 0.02f) // Add a threshold to prevent jitter
+            {
+                lastJoyStickPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            
+                return;
+            }
+            transform.position += deltaPosition;
+            lastJoyStickPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            
+        }
+        followJoyStick = rightIndexTrigger && rightHandTrigger;
 
         UnityEngine.Vector3 worldPosition = sphereTransform.position;
         UnityEngine.Vector3 localPos = jointTransform.InverseTransformPoint(worldPosition);
